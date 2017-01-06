6834b4834405fe289388eeec1361cd3c62682275
Jeremy Roberts <jaroberts@ksu.edu>
2017-01-06 16:19:59 -0600

    First real commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # state_map
-Plot state map given state-wise data.
+Plot state map given state-wise data. 
+
+Included:
+  fillstates.py -- a slight modification of the same file from basemap examples
+  st99_d00.dbf  -- attribute file
+  st99_d00.shp  -- shape file (i.e., the geometry)
+  st99_d00.shx  -- shape index
+
+Note, the latter st99 files come with the basemap examples but are themselves
+available from the USDA: 
+
+https://infosys.ars.usda.gov/svn/code/weps1/trunk/weps.install/db/gis/st99_d00/
+
+To run, you'll need to install basemap (which, if you use conda, is as 
+easy as conda install basemap).
+
+Mods by Jeremy Roberts (2017).  jaroberts@ksu.edu.
+
---
+++ fillstates.py
@@ -0,0 +1,130 @@
+# Adapted from fillstates.py from the basemap examples 
+
+from __future__ import print_function
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap as Basemap
+from matplotlib.colors import rgb2hex
+from matplotlib.patches import Polygon
+from matplotlib.colors import LinearSegmentedColormap
+
+popdensity = {
+    'New Jersey':  438.00,
+    'Rhode Island':   387.35,
+    'Massachusetts':   312.68,
+    'Connecticut':	  271.40,
+    'Maryland':   209.23,
+    'New York':    155.18,
+    'Delaware':    154.87,
+    'Florida':     114.43,
+    'Ohio':	 107.05,
+    'Pennsylvania':	 105.80,
+    'Illinois':    86.27,
+    'California':  83.85,
+    'Hawaii':  72.83,
+    'Virginia':    69.03,
+    'Michigan':    67.55,
+    'Indiana':    65.46,
+    'North Carolina':  63.80,
+    'Georgia':     54.59,
+    'Tennessee':   53.29,
+    'New Hampshire':   53.20,
+    'South Carolina':  51.45,
+    'Louisiana':   39.61,
+    'Kentucky':   39.28,
+    'Wisconsin':  38.13,
+    'Washington':  34.20,
+    'Alabama':     33.84,
+    'Missouri':    31.36,
+    'Texas':   30.75,
+    'West Virginia':   29.00,
+    'Vermont':     25.41,
+    'Minnesota':  23.86,
+    'Mississippi':	 23.42,
+    'Iowa':	 20.22,
+    'Arkansas':    19.82,
+    'Oklahoma':    19.40,
+    'Arizona':     17.43,
+    'Colorado':    16.01,
+    'Maine':  15.95,
+    'Oregon':  13.76,
+    'Kansas':  12.69,
+    'Utah':	 10.50,
+    'Nebraska':    8.60,
+    'Nevada':  7.03,
+    'Idaho':   6.04,
+    'New Mexico':  5.79,
+    'South Dakota':	 3.84,
+    'North Dakota':	 3.59,
+    'Montana':     2.39,
+    'Wyoming':      1.96,
+    'Alaska':     0.42}
+
+def make_map(state_data = popdensity, title='') :
+    """ Take in state-wise data in dictionary format and plot on map 
+        of U.S.
+    
+    """
+    plt.figure(1,figsize=(9,5))
+    # Lambert Conformal map of lower 48 states.
+    m = Basemap(llcrnrlon=-119,llcrnrlat=19,urcrnrlon=-64,urcrnrlat=49,
+                projection='lcc',lat_1=33,lat_2=45,lon_0=-95)
+    # draw state boundaries.
+    # data from U.S Census Bureau
+    # http://www.census.gov/geo/www/cob/st2000.html
+    shp_info = m.readshapefile('st99_d00','states',drawbounds=True)
+    # population density by state from
+    # http://en.wikipedia.org/wiki/List_of_U.S._states_by_population_density
+ 
+    # Segmented color scheme ranging from blue to red
+    cdict1 = {'red':   ((0.0, 0.0, 0.0),
+                       (1.0, 1.0, 1.0)),
+    
+             'green': ((0.0, 0.0, 0.0),
+                       (1.0, 0.0, 0.0)),
+    
+             'blue':  ((0.0, 1.0, 1.0),
+                       (1.0, 0.0, 0.0))
+            }
+    cmap = LinearSegmentedColormap('BlueRed1', cdict1)
+  
+    # choose a color for each state based on state-wise data.
+    colors={}
+    statenames=[]
+    vmin = min(state_data.values()) 
+    vmax = max(state_data.values())
+    for shapedict in m.states_info:
+        statename = shapedict['NAME']
+        # skip DC and Puerto Rico.
+        if statename not in ['District of Columbia','Puerto Rico']:           
+            pop = popdensity[statename]
+            # calling colormap with value between 0 and 1 returns
+            # rgba value.  Invert color range (hot colors are high
+            # population), take sqrt root to spread out colors more.
+            colors[statename] = cmap(1.-np.sqrt((pop-vmin)/(vmax-vmin)))[:3]
+        statenames.append(statename)
+    # cycle through state names, color each one.
+    ax = plt.gca() # get current axes instance
+    for nshape,seg in enumerate(m.states):
+        # skip DC and Puerto Rico.
+        if statenames[nshape] not in ['District of Columbia','Puerto Rico']:
+            if statenames[nshape] == 'Alaska':
+            # Alaska is too big. Scale it down to 35% first, then transate it. 
+                seg = list(map(lambda (x,y): (0.35*x + 1000000, 0.35*y-1300000), seg))
+            if statenames[nshape] == 'Hawaii':
+                seg = list(map(lambda (x,y): (x + 5100000, y-1500000), seg))
+            color = rgb2hex(colors[statenames[nshape]]) 
+            poly = Polygon(seg,facecolor=color,edgecolor='black')
+            ax.add_patch(poly)
+    plt.title(title)
+    # Hackish way to define color map
+    tmp=np.linspace(vmin, vmax,100)
+    im = plt.imshow(np.array([tmp, tmp]), cmap=cmap)
+    plt.colorbar(im)
+    plt.tight_layout()
+    #plt.axis([])
+    plt.show()
+
+if __name__ == '__main__' :
+    make_map(state_data = popdensity, title='Population Density')
